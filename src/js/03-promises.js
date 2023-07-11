793b9f97893030c9c6679ce3146fad5748f46f2d
DariiaKorovchenko <samurais.daughter007@gmail.com>
2023-07-11 16:22:36 +0300

    Done Task 3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,44 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+const form = document.querySelector('form');
+form.addEventListener('submit', onFormSubmit);
+form.addEventListener('input', onInputForm);
+
+const values = {};
+
+function onInputForm(event) {
+  values[event.target.name] = event.target.value;
+}
+
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const shouldResolve = Math.random() > 0.3;
+      if (shouldResolve) {
+        resolve(`✅ Fulfilled promise ${position} in ${delay}ms`);
+        console.dir(Promise);
+      } else {
+        reject(`❌ Rejected promise ${position} in ${delay}ms`);
+      }
+    }, delay);
+  });
+}
+
+function onFormSubmit(event) {
+  event.preventDefault();
+  let timeSum = Number.parseInt(values.delay);
+  let counter = 1;
+  createPromise(counter, timeSum)
+    .then(result => Notify.success(result))
+    .catch(error => Notify.failure(error));
+  counter += 1;
+  setInterval(() => {
+    if (counter <= values.amount) {
+      timeSum += Number.parseInt(values.step);
+      createPromise(counter, timeSum)
+        .then(result => Notify.success(result))
+        .catch(error => Notify.failure(error));
+      counter += 1;
+    }
+  }, 0);
 }
